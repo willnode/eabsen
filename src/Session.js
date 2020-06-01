550235911da7472896ae73f42a0f137b5b38fc38
Julius-745 <juliusagung123@gmail.com>
2020-06-01 16:04:11 +0700

    json

--- src/Session.js
+++ src/Session.js
@@ -1,6 +1,6 @@
 
 
-const serverUrl = process.env.NODE_ENV === 'production' ? 'https://dev.wellosoft.net/eabsen' : 'http://'+window.location.hostname+'/eabsen-ci';
+const serverUrl = process.env.NODE_ENV === 'production' ? 'https://api.wellosoft.net/eabsen' : 'http://'+window.location.hostname+'/eabsen-ci';
 
 const serverHandler = async (url,method,body) => {
 	let response;
