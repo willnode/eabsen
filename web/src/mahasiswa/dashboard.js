4ad148b07d0fcb8ec547abd446d73a539bcae6b2
Wildan M <willnode@wellosoft.net>
2020-06-22 23:53:35 +0700

    Update dashboard.js

--- web/src/mahasiswa/dashboard.js
+++ web/src/mahasiswa/dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Page } from '../widget/page';
 import { login } from '../main/Helper';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 export default function () {
 	return (
